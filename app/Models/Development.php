978d6b83a434c9337e4924db4770852fab09303d
riefky <2006085@itg.ac.id>
2025-11-07 10:44:33 +0700

    add model development

---
+++ app/Models/Development.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use App\traits\UUID;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Development extends Model
+{
+    use SoftDeletes, UUID;
+
+    protected $fillable = [
+        'thumbnail',
+        'name',
+        'description',
+        'person_in_charge',
+        'start_date',
+        'end_date',
+        'amount',
+        'status'
+    ];
+}
